20fb80658689903c9accfaa83cd6f592152bd150
Dinuka_prasad <127348943+DinukaPrasad@users.noreply.github.com>
2024-11-14 05:40:03 +0530

    update 11/14 our offering section updated

--- FFC.js
+++ FFC.js
@@ -1,6 +1,13 @@
- function changeImage(anything){
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
 
-    document.getElementById('slider').src = anything;
-    
-}
+next.addEventListener('click' , function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click' , function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1])
+})
       
